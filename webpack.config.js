0376efe7ed2c5871a982f73530e0668f061498f5
3daddict <print@3daddict.com>
2019-03-07 17:46:15 -0800

    webpack updates working on bp

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,69 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management"
+      hash: true,
+      title: 'My Awesome application',
+      myPageHeader: 'Hello World',
+      template: './src/index.html',
+      filename: './index.html' //relative to root of the application
     })
   ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      { 
+        //BabelJS Loader
+        test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" 
+      },
+      {
+        //SASS Loader
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      },
+      //Image Compiler
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      }
+    ]
   }
 };
